ba7f0b9558eb52956f55bd7e2119b152d50e7063
Kamilah Amir-Al-Sharif <kamilahamir-al-sharif@Kamilahs-MacBook-Pro.local>
2018-04-24 21:48:58 -0400

    adding url

---
+++ cards/card1.js
@@ -0,0 +1,23 @@
+/* point - the point value of the card: a number between 1 and 13.
+   suit - the suit of the card: one of diamonds, clubs, hearts and spades.*/
+
+function Card (point, suit){
+    this.point = point;
+    this.suit = suit;
+}
+
+var myCard = new Card(5, 'diamonds');
+console.log(myCard.point);
+console.log(myCard.suit);
+
+// push url element into the Card object
+// var getImage = {};
+// getImage.push(Card);
+
+Card.prototype.getImage = function(url){
+    return('cards/' + this.point + '_of_' + this.suit + '.png');
+}
+
+myCard.getImage(card);
+
+
